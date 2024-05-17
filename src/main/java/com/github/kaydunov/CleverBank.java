30f744767523450fbf738f3821088b4bb8fe124d
Denis Kaydunov <deniskaydunov@sap.com>
2024-05-17 14:25:33 +0300

    configured connection using ConnectionManager and application.yaml

---
+++ src/main/java/com/github/kaydunov/CleverBank.java
@@ -0,0 +1,10 @@
+package com.github.kaydunov;
+
+
+import javax.servlet.ServletException;
+
+public class CleverBank {
+    public static void main(String[] args) throws ServletException {
+        // Noncompliant - method is empty
+    }
+}
--- src/main/java/com/github/kaydunov/dao/ConnectionManager.java
+++ src/main/java/com/github/kaydunov/dao/ConnectionManager.java
@@ -1,36 +1,47 @@
 package com.github.kaydunov.dao;
 
 import com.github.kaydunov.exception.DatabaseConfigurationException;
+import lombok.extern.java.Log;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.Properties;
 
+@Log
 public class ConnectionManager {
     private static final Properties PROPERTIES = new Properties();
     private static final String DATABASE_URL;
 
     static {
-            try {
-                Class.forName("${db.driver}");
-            } catch (ClassNotFoundException e) {
-                throw new DatabaseConfigurationException("Incorrect database driver", e);
-            }
-        DATABASE_URL = "${db.url}";
+        String yamlFilePath = "src/main/resources/application.yaml";
+
+        // Load YAML file
+        Yaml yaml = new Yaml();
+        try {
+            FileInputStream inputStream = new FileInputStream(yamlFilePath);
+            Map<String, Object> yamlMap = yaml.load(inputStream);
+
+            // Get JDBC driver configuration
+           PROPERTIES.putAll((Map<String, String>) yamlMap.get("db"));
+
+            String driverName = PROPERTIES.getProperty("driver");
+            Class.forName(driverName);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new DatabaseConfigurationException("Incorrect database driver", e);
+        }
+        DATABASE_URL = PROPERTIES.getProperty("url");
+
     }
 
     private ConnectionManager(){}
 
-    private static class SingletonHelper {
-        private static final ConnectionManager INSTANCE = new ConnectionManager();
-    }
 
-    public static ConnectionManager getInstance() {
-        return SingletonHelper.INSTANCE;
-    }
-
-    public Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DATABASE_URL, PROPERTIES);
     }
 }
