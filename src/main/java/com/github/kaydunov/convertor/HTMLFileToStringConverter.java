d541fd87f333941f9fe7765bb6e9358cf644034a
Denis Kaydunov <deniskaydunov@sap.com>
2024-05-24 16:12:01 +0300

    added html convertor

---
+++ src/main/java/com/github/kaydunov/convertor/HTMLFileToStringConverter.java
@@ -0,0 +1,21 @@
+package com.github.kaydunov.convertor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HTMLFileToStringConverter {
+    public static String convertHTMLFileToString(String filePath) throws IOException {
+        StringBuilder htmlContent = new StringBuilder();
+
+        // Create a buffered reader to read the lines of the HTML file
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                htmlContent.append(line).append("\n");
+            }
+        }
+
+        return htmlContent.toString();
+    }
+}
