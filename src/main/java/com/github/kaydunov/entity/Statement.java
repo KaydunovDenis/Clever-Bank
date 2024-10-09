e17c0e703f66f25b0905b1f9acd9fa25bf73ac8b
Denis Kaydunov <deniskaydunov@gmail.com>
2024-10-09 17:57:44 +0300

    added Statement

---
+++ src/main/java/com/github/kaydunov/entity/Statement.java
@@ -0,0 +1,90 @@
+package com.github.kaydunov.entity;
+
+import com.github.kaydunov.util.StringUtils;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+/**
+ * This class responsible for a statement view.
+ * <p>
+ *
+ *                           Выписка
+ *                         Clever-Bank
+ * | Клиент                    | Сотников Кирилл Артёмович                |
+ * | Счет                      | AS12 ASDG 1200 2132 ASDA 353A 2132       |
+ * | Валюта                    | BYN                                      |
+ * | Дата открытия             | 01.01.1970                               |
+ * | Период                    | 01.01.1970 - 08.10.2024                  |
+ * | Дата и время формирования | 01.11.2024, 14:47                        |
+ * | Остаток                   | 10,000.00 BYN                            |
+ * | Дата       | Примечание           | Сумма      |
+ * ----------------------------------------------
+ * | 08.10.2024 | Снятие средств        |  -1000.10 BYN |
+ * | 08.10.2024 | Пополнение от Иванова |   2512.72 BYN |
+ * | 08.10.2024 | Пополнение от Петрова |    638.02 BYN |
+ *
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Statement {
+    String clientName;
+    String accountNumber;
+    String currency;
+    LocalDate accountOpeningDate;
+    LocalDate periodStartDate;
+    LocalDate periodEndDate;
+    LocalDateTime generationDate;
+    BigDecimal balance;
+    List<Transaction> transactions;
+
+    static final DateTimeFormatter onlyDateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm");
+    static final int WITH = 65;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(StringUtils.centerStringLn("Выписка", WITH));
+        fillAccountInfo(sb);
+        fillAccountTransactionsInfo(sb);
+        return sb.toString();
+    }
+
+    private void fillAccountInfo(StringBuilder sb) {
+        sb.append(StringUtils.centerStringLn("Clever-Bank", WITH));
+        sb.append(String.format(" Клиент                    | %-40s %n", clientName));
+        sb.append(String.format(" Счет                      | %-40s %n", accountNumber));
+        sb.append(String.format(" Валюта                    | %-40s %n", currency));
+        sb.append(String.format(" Дата открытия             | %-40s %n", accountOpeningDate.format(onlyDateFormat)));
+        sb.append(String.format(" Период                    | %s - %-27s %n",
+                periodStartDate.format(onlyDateFormat),
+                periodEndDate.format(onlyDateFormat)));
+        sb.append(String.format(" Дата и время формирования | %-40s %n",
+                generationDate.format(dateTimeFormatter)));
+        sb.append(String.format(" Остаток                   | %,.2f %-30s %n", balance, currency));
+    }
+
+    private void fillAccountTransactionsInfo(StringBuilder sb) {
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+        sb.append(String.format("%s| %-20s | %-10s %n", "    Дата    ", "Примечание", "Сумма"));
+        sb.append("-".repeat(WITH)).append("\n");
+        for (Transaction transaction : transactions) {
+            sb.append(String.format(" %-10s | %-20s | %-10.2f %s %n",
+                    dateFormat.format(transaction.getCreatedAt()),
+                    transaction.getTransactionType(),
+                    transaction.getAmount(),
+                    currency));
+        }
+    }
+
+}
+
---
+++ src/test/java/com/github/kaydunov/entity/StatementTest.java
@@ -0,0 +1,90 @@
+package com.github.kaydunov.entity;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Timeout(value = 5, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+public class StatementTest {
+
+    @Test()
+    void toStringTest() {
+        //Arrange Statement(s)
+        Statement target = getStatement();
+
+        //Act Statement(s)
+        String result = target.toString();
+        System.out.println("RESULT:\n" + result);
+
+        //Assert statement(s)
+        String expectedCheck = """
+                                             Выписка                            \s
+                                           Clever-Bank                          \s
+                 Клиент                    | Сотников Кирилл Артёмович               \s
+                 Счет                      | AS12 ASDG 1200 2132 ASDA 353A 2132      \s
+                 Валюта                    | BYN                                     \s
+                 Дата открытия             | 01.01.1970                              \s
+                 Период                    | 01.01.1970 - 08.10.2024                 \s
+                 Дата и время формирования | 01.11.2024, 14:47                       \s
+                 Остаток                   | 10,000.00 BYN                           \s
+                    Дата    | Примечание           | Сумма     \s
+                -----------------------------------------------------------------
+                 08.10.2024 | WITHDRAW             | 1000.00    BYN\s
+                 09.10.2024 | DEPOSIT              | 2512.72    BYN\s
+                 11.10.2023 | DEPOSIT              | 512.72     BYN\s
+                """;
+        assertEquals(expectedCheck, result);
+    }
+
+    @Disabled("Manual")
+    @Test
+    void test() {
+        System.out.println(getStatement());
+    }
+
+
+    public static Statement getStatement() {
+        Statement statement = new Statement();
+        statement.setClientName("Сотников Кирилл Артёмович ");
+        statement.setAccountNumber("AS12 ASDG 1200 2132 ASDA 353A 2132");
+        statement.setCurrency("BYN");
+        statement.setAccountOpeningDate(LocalDate.of(1970, 1, 1));
+        statement.setPeriodStartDate(LocalDate.of(1970, 1, 1));
+        statement.setPeriodEndDate(LocalDate.of(2024, 10, 8));
+        statement.setGenerationDate(LocalDateTime.of(2024, 11, 1, 14, 47));
+        statement.setBalance(BigDecimal.valueOf(10000));
+        statement.setTransactions(getTransactions());
+        return statement;
+    }
+
+    private static List<Transaction> getTransactions() {
+        Transaction transaction1 = new Transaction(
+                BigDecimal.valueOf(1000),
+                new Timestamp(Date.valueOf("2024-10-08").getTime()),
+                1L,
+                2L,
+                TransactionType.WITHDRAW);
+        Transaction transaction2 = new Transaction(
+                BigDecimal.valueOf(2512.72),
+                new Timestamp(Date.valueOf("2024-10-09").getTime()),
+                3L,
+                1L,
+                TransactionType.DEPOSIT);
+        Transaction transaction3 = new Transaction(
+                BigDecimal.valueOf(512.72),
+                new Timestamp(Date.valueOf("2023-10-11").getTime()),
+                3L,
+                1L,
+                TransactionType.DEPOSIT);
+        return List.of(transaction1, transaction2, transaction3);
+    }
+}
