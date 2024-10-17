bbf1897eb2e77c15310045f8a423cdc703579524
Denis Kaydunov <deniskaydunov@gmail.com>
2024-10-17 12:42:02 +0300

    turn on a charge percent processing

--- src/main/java/com/github/kaydunov/CleverBank.java
+++ src/main/java/com/github/kaydunov/CleverBank.java
@@ -1,5 +1,6 @@
 package com.github.kaydunov;
 
+import com.github.kaydunov.percentage_processor.PercentageProcessor;
 import com.github.kaydunov.servlet.TomcatManager;
 import com.github.kaydunov.spring.ApplicationContext;
 
@@ -7,6 +8,8 @@ public class CleverBank {
 
     public static void main(String[] args) {
         ApplicationContext context = new ApplicationContext(CleverBank.class);
+        PercentageProcessor percentageProcessor = context.getBean(PercentageProcessor.class);
+        percentageProcessor.startProcessing();
         TomcatManager.start(context);
     }
 }
