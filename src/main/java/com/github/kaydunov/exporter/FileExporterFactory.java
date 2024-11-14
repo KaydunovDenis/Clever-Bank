5464cf86d58dd99c33a46b0421f057a092d0483e
Denis Kaydunov <deniskaydunov@gmail.com>
2024-11-14 16:13:00 +0300

    added Fabric for FileExporter

--- src/main/java/com/github/kaydunov/exporter/FileExporterFactory.java
+++ src/main/java/com/github/kaydunov/exporter/FileExporterFactory.java
@@ -1,19 +1,29 @@
 package com.github.kaydunov.exporter;
 
+import com.github.kaydunov.exception.FileExporterException;
 import com.github.kaydunov.spring.Autowired;
+import com.github.kaydunov.spring.Component;
 
 import java.util.List;
 
+@Component
 public class FileExporterFactory {
 
     @Autowired
     private List<FileExporter> exporters;
 
-    public static FileExporter getExporter(String format) {
-        exporters.get
-        format.toLowerCase()
 
-    //default -> throw new IllegalArgumentException("Unsupported format: " + format);
-        };
+    public FileExporter getExporter(String extension) {
+        String beanName = extension.toLowerCase() + "exporter";
+        return exporters.stream()
+                .filter(exporter -> getExporterName(exporter).equals(beanName))
+                .findFirst()
+                .orElseThrow(() -> new FileExporterException("Unsupported format: " + extension));
     }
+
+    private String getExporterName(FileExporter exporter) {
+        return exporter.getClass().getSimpleName().toLowerCase();
+    }
+
 }
+
--- src/test/java/com/github/kaydunov/exporter/FileExporterFactoryTest.java
+++ src/test/java/com/github/kaydunov/exporter/FileExporterFactoryTest.java
@@ -1,19 +1,45 @@
 package com.github.kaydunov.exporter;
 
+import com.github.kaydunov.exception.FileExporterException;
+import com.github.kaydunov.spring.ApplicationContext;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileExporterFactoryTest {
 
+    private static ApplicationContext context;
+
+    private FileExporterFactory target;
+
+    @BeforeAll
+
+    static void initContext() {
+        context = new ApplicationContext(FileExporter.class);
+    }
+
+    @BeforeEach
+    void setUp() {
+        target = context.getBean(FileExporterFactory.class);
+    }
+
     @Test
-    void getExporter() {
-        FileExporter pdfExporter = FileExporterFactory.getExporter("pdf");
+    void shouldReturnPdfExporter() {
+        FileExporter pdfExporter = target.getExporter("pdf");
         assertTrue(pdfExporter instanceof PdfExporter);
+    }
 
-        FileExporter txtExporter = FileExporterFactory.getExporter("txt");
+    @Test
+    void shouldReturnTxtExporter() {
+        FileExporter txtExporter = target.getExporter("txt");
         assertTrue(txtExporter instanceof TxtExporter);
+    }
 
-        assertThrows(IllegalArgumentException.class, () -> FileExporterFactory.getExporter("unsupported"));
+    @Test
+    void shouldThrowExceptionForUnsupportedFormat() {
+        assertThrows(FileExporterException.class, () -> target.getExporter("unsupported"));
     }
 }
